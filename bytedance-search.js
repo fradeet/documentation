a9aa45de716d01f942f60457a7105f4df8aa5519
fradeet <41571697+fradeet@users.noreply.github.com>
2026-06-17 16:16:32 +0800

    Create bytedance-search.js

---
+++ bytedance-search.js
@@ -0,0 +1,7 @@
+(function(){
+var el = document.createElement("script");
+el.src = "https://lf1-cdn-tos.bytegoofy.com/goofy/ttzz/push.js?d54bebf1f4e7f4af80f84ac3798723f7d44ef0ea3e3103853e0418496f0b875319d1c501ebd3301f5e2290626f5b53d078c8250527fa0dfd9783a026ff3cf719";
+el.id = "ttzz";
+var s = document.getElementsByTagName("script")[0];
+s.parentNode.insertBefore(el, s);
+})(window)
