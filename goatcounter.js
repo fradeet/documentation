66ecc5b78f9c6bda4a85bf3f8b956370f4d50b87
fradeet <41571697+fradeet@users.noreply.github.com>
2026-06-18 15:01:18 +0800

    Fix GoatCounter CDN connection issue.

--- goatcounter.js
+++ goatcounter.js
@@ -1,7 +1,7 @@
 (function () {
   const script = document.createElement("script");
   script.async = true;
-  script.src = "https://gc.zgo.at/count.js";
+  script.src = "https://cdn.jsdelivr.net/gh/arp242/goatcounter/public/count.min.js";
   script.setAttribute(
     "data-goatcounter",
     "https://jeekmind-help.goatcounter.com/count"
